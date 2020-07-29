a631fd44a6e2c9dab5f9c7058f60d22cd6a76cd4
padavanocskos <davidbecsei@gmail.com>
2020-07-29 14:31:37 +0200

    Set access token for articles controller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,48 +1,50 @@
 module Api
   module V1
     class ArticlesController < ApplicationController
+      before_action :doorkeeper_authorize! # equivalent of authenticate_user!
+
       def index
         articles = Article.order('created_at DESC')
         render json: { status: 'SUCCESS', message: 'Loaded articles', data: articles }, status: :ok
       end
-    end
 
-    def show
-      article = Article.find(params[:id])
-      render json: { status: 'SUCCESS', message: 'Loaded article', data: article }, status: :ok
-    end
-
-    def create
-      article = Article.create(article_params)
-
-      if article.save
-        render json: { status: 'SUCCESS', message: 'Article saved', data: article }, status: :ok
-      else
-        render json: { status: 'ERROR', message: 'Article not saved', data: article.errors }, status: :unprocessable_entity
+      def show
+        article = Article.find(params[:id])
+        render json: { status: 'SUCCESS', message: 'Loaded article', data: article }, status: :ok
       end
-    end
 
-    def destroy
-      article = Article.find(params[:id])
-      article.destroy
+      def create
+        article = Article.create(article_params)
 
-      render json: { status: 'SUCCESS', message: 'Article destroyed', data: article }, status: :ok
-    end
-
-    def update
-      article = Article.find(params[:id])
-
-      if article.update_attributes(article_params)
-        render json: { status: 'SUCCESS', message: 'Article updated', data: article }, status: :ok
-      else
-        render json: { status: 'ERROR', message: 'Article not updated', data: article.errors }, status: :unprocessable_entity
+        if article.save
+          render json: { status: 'SUCCESS', message: 'Article saved', data: article }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Article not saved', data: article.errors }, status: :unprocessable_entity
+        end
       end
-    end
 
-    private
+      def destroy
+        article = Article.find(params[:id])
+        article.destroy
 
-    def article_params
-      params.permit(:title, :body)
+        render json: { status: 'SUCCESS', message: 'Article destroyed', data: article }, status: :ok
+      end
+
+      def update
+        article = Article.find(params[:id])
+
+        if article.update_attributes(article_params)
+          render json: { status: 'SUCCESS', message: 'Article updated', data: article }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Article not updated', data: article.errors }, status: :unprocessable_entity
+        end
+      end
+      
+      private
+
+      def article_params
+        params.permit(:title, :body)
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users
+
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications,
+      :authorized_applications
+  end
 
   namespace 'api' do
     namespace 'v1' do
